207fc01403ae07ccf3b3f39c0055a8c64ae81855
akashkumar.patel <akashkumar.patel@dcmail.ca>
2025-01-31 23:02:13 -0500

    add new dat:

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.get('/health', (req, res) => {
 
 // Default route
 app.get('/', (req, res) => {
-  res.json({ message: "Hello from Node.js backend new and old!" });
+  res.json({ message: "Hello from Node.js is dp!" });
 });
 
 // Set the port for the backend
