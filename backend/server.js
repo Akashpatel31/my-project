e126f0ed359a3b3addbdb89d2e57aac8308edb8c
akashkumar.patel <akashkumar.patel@dcmail.ca>
2025-02-01 22:39:36 -0500

    replace code of yml

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.get('/health', (req, res) => {
 
 // Default route
 app.get('/', (req, res) => {
-  res.json({ message: "Hello from Node.js is dp!" });
+  res.json({ message: "Hello from Node.js is dp is c!" });
 });
 
 // Set the port for the backend
